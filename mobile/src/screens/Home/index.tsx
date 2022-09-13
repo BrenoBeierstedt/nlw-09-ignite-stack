eadf74ba4c06dad1f710340efc7ef51836ef96a1
Breno Beierstedt <breno.beierstedt@hotmail.com>
2022-09-13 18:28:58 -0300

    Adding mobile project & exposing vite (web) port

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,36 @@
+import {Image, Text, View, FlatList} from "react-native";
+import { styles } from "./styles";
+import { GAMES } from '../../utils/games'
+import logo from "../../assets/logo-nlw-esports.png"
+import {Heading} from "../../components/Heading";
+import {GameCard} from "../../components/GameCard";
+export function Home() {
+
+    return (
+        <View style={styles.container}>
+            <Image
+                source={logo}
+                style={styles.logo}
+            />
+            <Heading
+                title="Encontre seu duo!"
+                subtitle="Selecione o game que deseja jogar..."
+            />
+            <FlatList
+                data={GAMES}
+                keyExtractor={item => item.id}
+                renderItem={({item})=>(
+                    <GameCard
+                        data={item}
+                    />
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={styles.contentList}
+            >
+
+            </FlatList>
+
+        </View>
+    )
+}
